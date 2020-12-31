334168cf722b4777dbbe0ae47b47ac7e1c5d41eb
psivakrishnareddy <p.sivakrishnareddy.siva@gmail.com>
2020-12-31 10:43:03 +0530

    cors enabled, filter modified

--- src/main/java/com/auth/config/SecurityConfig.java
+++ src/main/java/com/auth/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.auth.config;
 
 import javax.servlet.Filter;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,6 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.auth.service.CustomUserDetailsService;
 
@@ -31,15 +36,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().disable().csrf().disable()
-		.authorizeRequests()
-		.antMatchers("/token").permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
+
+    	// Enable CORS and disable CSRF
+        http = http.cors().and().csrf().disable();
+		
+		// Set session management to stateless For Rest apis
+        http = http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and();
+		
+		// Set unauthorized requests exception handler
+        http = http
+            .exceptionHandling()
+            .authenticationEntryPoint(
+                (request, response, ex) -> {
+                    response.sendError(
+                        HttpServletResponse.SC_UNAUTHORIZED,
+                        ex.getMessage()
+                    );
+                }
+            )
+            .and();
+		
+		 // Set permissions on endpoints
+        http.authorizeRequests()
+            // Our public endpoints
+            .antMatchers("/api/public/**").permitAll()
+            .antMatchers("/token").permitAll()
+            .antMatchers(HttpMethod.GET, "/api/author/**").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/author/search").permitAll()
+            // Our private endpoints
+            .anyRequest().authenticated();
+		
+        //Add the filter to Check/authenticate for token on request
 		http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 	}
+	
+	
+	
+	
 	
 	
 	@Override
@@ -47,6 +84,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		auth.userDetailsService(customUserDetailsService).passwordEncoder(getPasswordEncoder());
 	}
 	
+	// Used By Spring if Cors is enabled.
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+	
+	
+    
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 //		return new BCryptPasswordEncoder();
--- src/main/java/com/auth/helper/JwtUtil.java
+++ src/main/java/com/auth/helper/JwtUtil.java
@@ -21,28 +21,38 @@ public class JwtUtil {
 	public String getUsernameFromToken(String token) {
 		return getClaimFromToken(token, Claims::getSubject);
 	}
+	
+	
 	//retrieve expiration date from jwt token
 	public Date getExpirationDateFromToken(String token) {
 		return getClaimFromToken(token, Claims::getExpiration);
 	}
+	
+	
 	public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
 		final Claims claims = getAllClaimsFromToken(token);
 		return claimsResolver.apply(claims);
 	}
+	
     //for retrieveing any information from token we will need the secret key
 	private Claims getAllClaimsFromToken(String token) {
 		return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
 	}
+	
 	//check if the token has expired
 	private Boolean isTokenExpired(String token) {
 		final Date expiration = getExpirationDateFromToken(token);
 		return expiration.before(new Date());
 	}
+	
+	
 	//generate token for user
 	public String generateToken(UserDetails userDetails) {
 		Map<String, Object> claims = new HashMap<>();
 		return doGenerateToken(claims, userDetails.getUsername());
 	}
+	
+	
 	//while creating the token -
 	//1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
 	//2. Sign the JWT using the HS512 algorithm and secret key.
@@ -53,6 +63,8 @@ public class JwtUtil {
 				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
 				.signWith(SignatureAlgorithm.HS512, SECRET_KEY).compact();
 	}
+	
+	
 	//validate token
 	public Boolean validateToken(String token, UserDetails userDetails) {
 		final String username = getUsernameFromToken(token);
